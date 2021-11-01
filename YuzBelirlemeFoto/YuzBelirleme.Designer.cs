3d06853f021a2bb973bf574bab05b5fda9d35c6d
Ömer Faruk Ekrem <omerfarukekrem@outlook.com>
2021-11-01 15:42:23 +0300

    Add project files.

---
+++ YuzBelirlemeFoto/YuzBelirleme.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace YuzBelirlemeFoto
+{
+    partial class YuzBelirleme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img
+            // 
+            this.img.Location = new System.Drawing.Point(12, 12);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(410, 370);
+            this.img.TabIndex = 0;
+            this.img.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Algıla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.img);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ YuzBelirlemeFoto/YuzBelirleme.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.MachineLearning;
+using OpenCvSharp.Blob;
+using OpenCvSharp.UserInterface;
+using OpenCvSharp.CPlusPlus;
+using System.Diagnostics;
+
+namespace YuzBelirlemeFoto
+{
+    public partial class YuzBelirleme : Form
+    {
+        public YuzBelirleme()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap image = (Bitmap)Bitmap.FromFile(@"C:\Users\omer_ekrem\Desktop\resim.jpg");
+            IplImage imageipl = BitmapConverter.ToIplImage(image);
+            img.Image = BitmapConverter.ToBitmap(scanFace(imageipl));
+        }
+
+        private IplImage scanFace(IplImage resim) 
+        {
+            CvColor face = new CvColor(0, 0, 255);
+            const double Scale = 3.0;   //hizli islem icin resmin kac kat kucultulecegi
+            const double ScaleFactor = 2.5;
+            const int MinNeighbors = 1;
+
+            using (IplImage imgMinimize = new IplImage(new CvSize(Cv.Round(resim.Width / Scale), Cv.Round(resim.Height / Scale)), BitDepth.U8, 1))
+            {
+
+                using (IplImage gri = new IplImage(resim.Size, BitDepth.U8, 1))   //renksizlestir
+                {
+                    Cv.CvtColor(resim, gri, ColorConversion.BgrToGray);
+                    Cv.Resize(gri, imgMinimize, Interpolation.Linear);
+                    Cv.EqualizeHist(imgMinimize, imgMinimize);
+                }
+                IplImage outImage = Cv.Clone(resim);
+
+
+                //asil tespit kismina geldik
+
+
+                using (CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile(Environment.CurrentDirectory + "\\haarcascade_frontalface_alt_tree.xml"))
+
+                using (CvMemStorage storage = new CvMemStorage())
+
+
+                {
+                    storage.Clear();
+                    Stopwatch watch = Stopwatch.StartNew();
+                    CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(imgMinimize, cascade, storage, ScaleFactor, MinNeighbors, 0, new CvSize(30, 30));
+                    watch.Stop();
+
+                    for (int i = 0; i < faces.Total; i++)
+
+                    {
+
+                        CvRect r = faces[i].Value.Rect;
+
+
+                        CvPoint center = new CvPoint
+
+
+                        {
+
+
+                            X = Cv.Round((r.X + r.Width * 0.5) * Scale),
+
+
+                            Y = Cv.Round((r.Y + r.Height * 0.5) * Scale)
+
+
+                        };
+
+
+                        int radius = Cv.Round((r.Width + r.Height) * 0.25 * Scale);
+
+
+                        resim.Circle(center, radius, face, 3, LineType.AntiAlias, 0);
+
+                    }
+
+                }
+                return resim;
+            }
+
+        }
+    }
+}
